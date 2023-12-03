76d34d84c8921836afc9bb86644fe78767755d14
chris <chris.nelson@campspot.com>
2023-12-03 00:57:39 -0700

    day 3!

---
+++ 2023/3/helpers.ts
@@ -0,0 +1,53 @@
+import LineByLine from "n-readlines";
+import nReadlines from "n-readlines";
+
+export function parseInput(file: string): string[] {
+    const lines = new nReadlines(file);
+
+    let engine: string[] = []
+    let line;
+
+    while (line = lines.next()) {
+        engine.push(line.toString())
+    }
+
+    return engine
+}
+
+export function findCandidatePartNumbers(engine: string[]) {
+    let partNumbers: PartNumber[] = []
+
+    for(let x= 0; x < engine.length; x++) {
+        let rawPartNumbers = engine[x].matchAll(/[0-9]+/g)
+        for(const rawPartNumber of rawPartNumbers!) {
+            partNumbers.push({
+                row: x,
+                start: rawPartNumber.index!,
+                end: rawPartNumber.index! + rawPartNumber[0].length - 1,
+                value: parseInt(rawPartNumber.pop()!)
+            })
+        }
+    }
+
+    return partNumbers;
+}
+
+export function isSymbolAdjacent(engine: string[], maybePartNumber: PartNumber) {
+    let rows = engine.length - 1
+    let cols = engine[0].length - 1
+
+    for (let r = Math.max(0, maybePartNumber.row - 1); r <= Math.min(rows, maybePartNumber.row + 1); r++){
+        for(let c = Math.max(0, maybePartNumber.start - 1); c <= Math.min(cols, maybePartNumber.end + 1); c++) {
+            if (engine[r][c].match(/[0-9.]/) == null) return true
+        }
+    }
+
+    return false
+}
+
+export interface PartNumber {
+    row: number
+    start: number
+    end: number
+    value: number
+}
---
+++ 2023/3/q1.ts
@@ -0,0 +1,23 @@
+import {findCandidatePartNumbers, isSymbolAdjacent, parseInput, PartNumber} from "./helpers";
+
+let partNumbers: PartNumber[] = []
+
+let engine: string[] = parseInput('2023/3/input.txt')
+let maybePartNumbers: PartNumber[] = findCandidatePartNumbers(engine)
+
+maybePartNumbers.forEach(maybePartNumber => {
+    if (isSymbolAdjacent(engine, maybePartNumber)) partNumbers.push(maybePartNumber)
+})
+
+let sumOfPartNumbers = partNumbers.reduce(
+    (acc, cur) => {
+        return acc + (cur.value)
+    },
+    0,
+);
+
+console.log(sumOfPartNumbers)
+
+
+
+
