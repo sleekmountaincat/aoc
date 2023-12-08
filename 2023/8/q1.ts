770595d9f6a5b40a48f548f0692346e96fae8ada
chris <chris.nelson@campspot.com>
2023-12-07 23:43:04 -0700

    day 8!

--- 2023/8/q1.ts
+++ 2023/8/q1.ts
@@ -16,25 +16,13 @@ input.forEach(i => {
     desertMap.set(loc, {L, R})
 })
 
-// let currentPoint = desertMap.entries().next().value[1][konamiCode[0]];
 let currentPoint = konamiCode[0] === "L" ? desertMap.get("AAA")!.L : desertMap.get("AAA")!.R
 let steps = 1
 
 do {
-    // let xs = konamiCode[steps % konamiCode.length]
-    // console.log(konamiCode[steps % konamiCode.length])
     let dest = desertMap.get(currentPoint)!
     currentPoint = konamiCode[steps % konamiCode.length] === "L" ? dest.L : dest.R
     steps++
 } while (currentPoint != "ZZZ")
 
 console.log(steps)
-
-// let desert
-
-
-// LLR
-//
-// AAA = (BBB, BBB)
-// BBB = (AAA, ZZZ)
-// ZZZ = (ZZZ, ZZZ)
