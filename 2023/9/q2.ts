a24d9c96e2a99f8f2b158196b752f242b1a56fc1
chris <chris.nelson@campspot.com>
2023-12-09 00:49:46 -0700

    day 9!

---
+++ 2023/9/q2.ts
@@ -0,0 +1,30 @@
+import fs from 'fs';
+
+let input: number[][] = fs.readFileSync('2023/9/input.txt')
+    .toString()
+    .split("\n")
+    .map(l => [...l.match(/[0-9-]+/g)!].map(n => +n))
+
+const adif = (seq: number[]): number[] => {
+    let dif: number[] = []
+    for (let x = 0; x < seq.length; x++) if (x+1 < seq.length) dif.push(seq[x+1] - seq[x])
+    return dif
+}
+
+const addem = (seq: number[]): number => seq.reduce((a, c) => a+c)
+const deaddem = (seq: number[]): number => seq.reduce((a, c) => c-a)
+
+let sum = addem(input.map(seq => {
+    let last: number[] = []
+
+    do {
+        last.push((seq[0]))
+        seq = adif(seq)
+    } while (!seq.every(n => n === 0))
+
+    last = last.reverse()
+
+    return deaddem(last)
+}))
+
+console.log(sum)
